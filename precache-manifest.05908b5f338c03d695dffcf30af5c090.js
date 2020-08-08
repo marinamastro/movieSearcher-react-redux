1cff0a61673ce5ef11485470c22241794e7a596d
Marina Mastropierro <marinamastropierro8@gmail.com>
2020-08-08 17:32:16 -0300

    Updates

---
+++ precache-manifest.05908b5f338c03d695dffcf30af5c090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e09fc5a4002392a3fee16be569617c",
+    "url": "/movieSearcher-react-redux/index.html"
+  },
+  {
+    "revision": "cb79f1b57f474d08a07d",
+    "url": "/movieSearcher-react-redux/static/css/main.21a5d953.chunk.css"
+  },
+  {
+    "revision": "8e7e9b5f6b21e1ad6a59",
+    "url": "/movieSearcher-react-redux/static/js/2.e2d30230.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movieSearcher-react-redux/static/js/2.e2d30230.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb79f1b57f474d08a07d",
+    "url": "/movieSearcher-react-redux/static/js/main.0b5032b0.chunk.js"
+  },
+  {
+    "revision": "3fb9d73f84c3b0caf013",
+    "url": "/movieSearcher-react-redux/static/js/runtime-main.4a1728e4.js"
+  }
+]);
