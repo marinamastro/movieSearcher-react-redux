514afe4f48fa43cdc14417a562def4e772be74ab
Marina Mastropierro <marinamastropierro8@gmail.com>
2020-08-19 12:39:52 -0300

    api key

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,8 @@
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
 export function getMovies(titulo) {
     return function(dispatch) {
-      return fetch("https://api.themoviedb.org/3/search/movie?api_key=00a34788f5928d2f5dc907d4549dc1d3&query=" + titulo)
+      return fetch("https://api.themoviedb.org/3/search/movie?api_key="+API_KEY+"&query=" + titulo)
         .then(response => response.json())
         .then(json => {           
           dispatch({ type: "GET_MOVIES", payload: json });
@@ -21,7 +23,7 @@ export function getMovies(titulo) {
 }
 export function movieDetail(id) {
     return function(dispatch) {
-      return fetch("https://api.themoviedb.org/3/movie/"+id+"?api_key=00a34788f5928d2f5dc907d4549dc1d3")
+      return fetch("https://api.themoviedb.org/3/movie/"+id+"?api_key="+API_KEY)
         .then(response => response.json())
         .then(json => {       
           dispatch({ type: "MOVIE_DETAIL", payload: json });
